c9dd3a7b4c1ab62c0717f5b42fd31dbaa81ab449
myahia93 <mohcine.93.yahia@gmail.com>
2024-01-07 12:56:37 +0100

    nmap updated

--- tools/nmap/nmap_functions.py
+++ tools/nmap/nmap_functions.py
@@ -8,19 +8,16 @@ def get_user_ip_input():
         user_input = input(
             "Enter the IP address or network (in format x.x.x.x or x.x.x.x/x): ")
         try:
-            ipaddress.IPv4Network(user_input)  # Try to parse as a network
-        except ipaddress.AddressValueError:  # If it fails, try to parse as an IP address
-            try:
+            if '/' in user_input:
+                ipaddress.IPv4Network(user_input)  # Try to parse as a network
+            else:
+                # Try to parse as an IP address
                 ipaddress.IPv4Address(user_input)
-                return user_input
-            except ipaddress.AddressValueError:
-                os.system("clear")
-                print(
-                    "\033[91mInvalid input. Please enter a valid IP address or network address.\033[0m")
-        except ipaddress.NetmaskValueError:
+            return user_input
+        except (ipaddress.AddressValueError, ipaddress.NetmaskValueError):
             os.system("clear")
             print(
-                "\033[91mInvalid input. Please enter a valid network address.\033[0m")
+                "\033[91mInvalid input. Please enter a valid IP address or network address.\033[0m")
 
 
 # Final nmap scan
