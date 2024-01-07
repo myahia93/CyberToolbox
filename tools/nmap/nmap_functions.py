1a91dfde3d34f371715a9c593131f0fe1b0df545
myahia93 <mohcine.93.yahia@gmail.com>
2024-01-07 15:57:02 +0100

    nmap updated

--- tools/nmap/nmap_functions.py
+++ tools/nmap/nmap_functions.py
@@ -22,17 +22,17 @@ def get_user_ip_input():
 
 # Nmap Options :
 
-def get_additional_options_menu(option_used=""):
+def get_additional_options_menu():
     additional_options = ""
     while True:
-        print("\n[1]> Port Filtering" +
-              (f" ({option_used})" if option_used else ""))
+        print("\nChoose an option :")
+        print("\n[1]> Port Filtering")
         print("[0]> No additional option (continue to scan)")
 
         try:
             option = int(input("Enter your option: "))
             if option == 1:
-                additional_options += get_port_filtering_options(option_used)
+                additional_options += get_port_filtering_options()
             elif option == 0:
                 break
             else:
@@ -41,32 +41,22 @@ def get_additional_options_menu(option_used=""):
             print(
                 "\033[91mInvalid input. Please enter a valid number.\033[0m", end='')
             print()  # Adds a line to separate the error message from the prompt)
-
-    if additional_options:
-        print("\nWould you like to choose another option? (y/n)")
-        user_choice = input("Your choice: ").lower()
-        if user_choice != 'y':
-            return additional_options  # Exit if the user does not want to choose another option
     return additional_options
 
 
 def get_port_filtering_options(option_used=""):
-    os.system("clear")
-    print("\n[1]> Most common ports (-F)" +
-          (f" {option_used}" if "Most common ports (-F)" in option_used else ""))
-    print("[2]> Specify port or port range (-p)" +
-          (f" {option_used}" if "Specify port or port range (-p)" in option_used else ""))
-    print("[3]> Scan all ports (-p-)" +
-          (f" {option_used}" if "Scan all ports (-p-)" in option_used else ""))
+    print("\n[1]> Most common ports (HTTP, SSH, Telnet, DNS, FTP...)")
+    print("[2]> Specify port or port range (in format xx or xx-xx)")
+    print("[3]> Scan all ports")
     print("[0]> Return to previous menu")
 
     try:
         option = int(input("Enter your option: "))
-        if option == 1 and "Most common ports (-F)" not in option_used:
+        if option == 1:
             return "-F "
-        elif option == 2 and "Specify port or port range (-p)" not in option_used:
+        elif option == 2:
             return f"-p {input('Enter port or port range:')} "
-        elif option == 3 and "Scan all ports (-p-)" not in option_used:
+        elif option == 3:
             return "-p- "
         elif option == 0:
             return ""
