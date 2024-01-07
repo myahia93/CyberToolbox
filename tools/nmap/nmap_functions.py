216eea4c410ff9122048cac9dcffc1469d3f4007
myahia93 <mohcine.93.yahia@gmail.com>
2024-01-07 14:55:36 +0100

    nmap updated

--- tools/nmap/nmap_functions.py
+++ tools/nmap/nmap_functions.py
@@ -20,7 +20,56 @@ def get_user_ip_input():
             print()  # Adds a line to separate the error message from the prompt
 
 
+def get_additional_options():
+    while True:
+        print("\n[1]> Port Filtering Options")
+        print("[0]> Return to main menu")
+
+        try:
+            option = int(input("Enter your option: "))
+            if option == 1:
+                return get_port_filtering_options()
+            elif option == 0:
+                return None
+            else:
+                os.system("clear")
+                print("\033[91mInvalid option. Please enter 0 or 1.\033[0m")
+        except ValueError:
+            os.system("clear")
+            print(
+                "\033[91mInvalid input. Please enter a valid number.\033[0m", end='')
+            print()  # Adds a line to separate the error message from the prompt
+
+
+def get_port_filtering_options():
+    print("\n[1]> Scan the most common ports (-F)")
+    print("[2]> Scan a specific port or range of ports (-p)")
+    print("[3]> Scan all ports (-p-)")
+    print("[0]> Return to additional options")
+
+    while True:
+        try:
+            option = int(input("Enter your option: "))
+            if option == 1:
+                return "-F"
+            elif option == 2:
+                return "-p " + input("Enter the port or range of ports: ")
+            elif option == 3:
+                return "-p-"
+            elif option == 0:
+                return None
+            else:
+                os.system("clear")
+                print(
+                    "\033[91mInvalid option. Please enter 0, 1, 2, or 3.\033[0m")
+        except ValueError:
+            os.system("clear")
+            print(
+                "\033[91mInvalid input. Please enter a valid number.\033[0m", end='')
+            print()  # Adds a line to separate the error message from the prompt
+
+
 # Final nmap scan
-def nmap_scan(ip_or_network):
-    nmap_command = f"nmap {ip_or_network}"
+def nmap_scan(ip_or_network, additional_options):
+    nmap_command = f"nmap {additional_options} {ip_or_network}"
     os.system(nmap_command)
--- tools/nmap/nmap_menu.py
+++ tools/nmap/nmap_menu.py
@@ -1,5 +1,5 @@
 import os
-from .nmap_functions import get_user_ip_input, nmap_scan
+from .nmap_functions import get_user_ip_input, nmap_scan, get_additional_options
 
 
 def nmap_title(print_banner_function):
@@ -26,7 +26,9 @@ def nmap_menu(print_banner_function):
             if option == 1:
                 ip_or_network = get_user_ip_input()
                 if ip_or_network:
-                    nmap_scan(ip_or_network)
+                    additional_options = get_additional_options()
+                    if additional_options:
+                        nmap_scan(ip_or_network, additional_options)
             elif option == 0:
                 return  # Retourne au menu principal
             else:
@@ -34,4 +36,6 @@ def nmap_menu(print_banner_function):
                 print("\033[91mInvalid option. Please enter 0 or 1.\033[0m")
         except ValueError:
             os.system("clear")
-            print("\033[91mInvalid input. Please enter a valid number.\033[0m")
+            print(
+                "\033[91mInvalid input. Please enter a valid number.\033[0m", end='')
+            print()
