be138a91e5f634531f405727c19d7f47f032b16f
myahia93 <mohcine.93.yahia@gmail.com>
2024-01-07 12:39:01 +0100

    nmap updated

--- tools/nmap/nmap_functions.py
+++ tools/nmap/nmap_functions.py
@@ -8,23 +8,23 @@ def get_user_ip_input():
         option = int(input(
             "[1]> Scan a Network\n[2]> Scan an IP Address\n[0]> Return to main menu\nEnter your option: "))
 
-        user_input_prompt = "Enter the network (in format x.x.x.x/x): " if option == 1 else "Enter the IP address: "
-        validation_error_message = "Invalid network address. Please enter a valid network address." if option == 1 else "Invalid IP address. Please enter a valid IP address."
-
         if option == 0:
             return None
 
-        while True:
-            user_input = input(user_input_prompt)
-            try:
-                if option == 1:
-                    ipaddress.IPv4Network(user_input)
-                else:
-                    ipaddress.IPv4Address(user_input)
-                return user_input
-            except (ipaddress.AddressValueError, ipaddress.NetmaskValueError):
-                os.system("clear")
-                print(f"\033[91m{validation_error_message}\033[0m")
+        user_input_prompt = "Enter the network (in format x.x.x.x/x): " if option == 1 else "Enter the IP address: "
+        validation_error_message = "Invalid network address. Please enter a valid network address." if option == 1 else "Invalid IP address. Please enter a valid IP address."
+
+        user_input = input(user_input_prompt)
+
+        try:
+            if option == 1:
+                ipaddress.IPv4Network(user_input)
+            else:
+                ipaddress.IPv4Address(user_input)
+            return user_input
+        except (ipaddress.AddressValueError, ipaddress.NetmaskValueError):
+            os.system("clear")
+            print(f"\033[91m{validation_error_message}\033[0m")
 
 
 # Final nmap scan
