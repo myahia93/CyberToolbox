cafc69c144a47a36dab57820d88c6529a8a2337e
myahia93 <mohcine.93.yahia@gmail.com>
2024-01-13 00:16:46 +0100

    sqlmap updated

--- main.py
+++ main.py
@@ -29,10 +29,10 @@ def main():
         print("\n[\033[92m1\033[0m]> \033[96mNmap\033[0m")
         print("[\033[92m2\033[0m]> \033[96mSQLmap\033[0m")
         print("[\033[92m3\033[0m]> \033[96mHydra (Bruteforce)\033[0m")
-        print("[\033[91m0\033[0m]> \033[96mExit the program\033[0m")
+        print("[\033[91m0\033[0m]> \033[91mExit the program\033[0m")
 
         try:
-            option = int(input("Enter your option: "))
+            option = int(input("\nEnter your option: "))
             if option == 1:
                 nmap_menu(print_banner)
             elif option == 2:
