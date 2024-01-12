173333b6780abc50ab4f40be60abd5f53085ff1c
myahia93 <mohcine.93.yahia@gmail.com>
2024-01-12 14:44:01 +0100

    sqlmap updated

--- tools/sqlmap/sqlmap_functions.py
+++ tools/sqlmap/sqlmap_functions.py
@@ -73,8 +73,8 @@ def process_sqlmap_output(sqlmap_output):
     db_start_index = sqlmap_output.find("available databases")
     if db_start_index != -1:
         db_lines = sqlmap_output[db_start_index:].split('\n')[2:-2]
-        formatted_result['Databases'] = [
-            line.split(']')[1].strip() for line in db_lines]
+        formatted_result['Databases'] = [line.split(']')[1].strip(
+        ) if ']' in line else line.strip() for line in db_lines]
 
     return formatted_result
 
